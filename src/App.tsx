8de32b03867b99712e03ed4d1ae8c05a3a13d31b
david <david.m.willett@gmail.com>
2021-02-26 15:45:12 +0000

    Added horseshoe list icons.

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,25 @@ function HorseAnimation() {
 function InstructionPanel() {
   return (
     <div className="Panel">
-      <p>Instruction Panel</p>
+      <h2>Instructions</h2>
+      <ul>
+        <li>
+          <span>Start by clicking the "Choose file" button above. A file selection window will open.</span>
+        </li>
+        <li>
+        <span>Browse to the folder containing the shift rota file (e.g. "Shifts.xlsx") and select that file. Click
+        "Open". The file selection window will close, and you will see the name of the selected file next to the
+        "Choose file" button.</span>
+        </li>
+        <li>
+        <span>Now click the "Enter sheet name" input field, and enter the name of the sheet to be planned, e.g.
+        "25-01-2021".</span>
+        </li>
+        <li>
+        <span>Click the "Upload file" button. Wait for about 5 seconds while HORSE plans the rota for the sheet
+        you chose.</span>
+        </li>
+      </ul>
     </div>
   );
 }
