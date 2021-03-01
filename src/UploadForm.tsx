aa85c8f1be0e5f72a3dd2df2e191cdd46ceaf9ff
david <david.m.willett@gmail.com>
2021-03-01 17:18:24 +0000

    Upload form updates sheet choice depending on file selection.

--- src/UploadForm.tsx
+++ src/UploadForm.tsx
@@ -7,23 +7,43 @@ type UploadFormProps = {
   disabled: boolean,
 }
 
-class UploadForm extends React.Component<UploadFormProps> {
+type UploadFormState = {
+  sheetOptions: { value: string, label: string }[],
+  selectedOption: { value: string, label: string } | null,
+}
+
+class UploadForm extends React.Component<UploadFormProps, UploadFormState> {
   private readonly fileInput: React.RefObject<HTMLInputElement>;
   private readonly rotaDoc: Rota.Document;
 
   constructor(props: UploadFormProps) {
     super(props);
+    this.state = {
+      sheetOptions: [],
+      selectedOption: null,
+    }
     this.fileInput = React.createRef();
     this.rotaDoc = new Rota.Document();
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleFileInputChange = this.handleFileInputChange.bind(this);
+    this.handleSheetChange = this.handleSheetChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleFileInputChange(event: React.FormEvent<HTMLInputElement>) {
     const file = event.currentTarget.files?.[0];
     if (file) {
-      this.rotaDoc.load(file).then(sheetNames => console.log(sheetNames));
+      this.rotaDoc.load(file).then(sheetNames =>
+        this.setState({
+          sheetOptions: sheetNames.map(sheetName =>
+            ({value: sheetName, label: sheetName})),
+          selectedOption: null,
+        })
+      );
     }
+  }
+
+  handleSheetChange(option: { value: string, label: string } | null) {
+    this.setState({selectedOption: option})
   }
 
   handleSubmit(event: React.FormEvent) {
@@ -41,7 +61,11 @@ class UploadForm extends React.Component<UploadFormProps> {
           </label>
           <label>
             Sheet to be planned:
-            <Select />
+            <Select
+              options={this.state.sheetOptions}
+              value={this.state.selectedOption}
+              onChange={this.handleSheetChange}
+              placeholder="Select sheet"/>
           </label>
           <button type="submit">
             Upload file
