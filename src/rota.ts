0f456cd296f22c3c29253dce11dff55882ab484f
david <david.m.willett@gmail.com>
2021-03-04 10:15:19 +0000

    Added Shift enum and duty fields.

--- src/rota.ts
+++ src/rota.ts
@@ -1,4 +1,4 @@
-import Excel from "exceljs";
+import Excel, {CellValue} from "exceljs";
 import {Enumify} from "enumify";
 
 export class Document {
@@ -47,10 +47,29 @@ class Table {
 class Record {
   private readonly teamField: Field;
   private readonly nameField: Field;
+  private readonly fishField: Field;
+  private readonly ssField: Field;
+  private readonly dsFields: Field[];
+  private readonly lateDSFields: Field[];
 
   constructor(columns: Columns, row: Excel.Row) {
     this.teamField = new Field(row.getCell(columns.team));
     this.nameField = new Field(row.getCell(columns.name));
+    this.fishField = new Field(row.getCell(columns.fish));
+    this.ssField = new Field(row.getCell(columns.ss));
+
+    this.dsFields = Array(Shift.enumValues.length);
+    // @ts-ignore
+    for (const shift of Shift) {
+      this.dsFields[shift.enumOrdinal] = new Field(row.getCell(columns.ds(shift)));
+    }
+
+    this.lateDSFields = Array(Shift.enumValues.length / 2);
+    // @ts-ignore
+    for (const shift of Shift) {
+      if (shift.enumOrdinal % 2 === 0) continue;
+      this.lateDSFields[Math.trunc(shift.enumOrdinal / 2)] = new Field(row.getCell(columns.lateDS(shift)));
+    }
   }
 }
 
@@ -65,10 +84,45 @@ class Field {
 class Columns {
   team = 1;
   name = 2;
+  fish: number;
+  ss: number;
+
+  private readonly firstDSColumn: number;
+  private readonly firstLateDSColumn: number;
 
   constructor(sheet: Excel.Worksheet) {
-
+    this.fish = Columns.findColumn(sheet, "FISH");
+    this.ss = Columns.findColumn(sheet, "SS");
+    this.firstDSColumn = Columns.findColumn(sheet, "DS");
+    this.firstLateDSColumn = Columns.findColumn(sheet, "Late DS");
   }
+
+  ds(shift: Shift): number {
+    return this.firstDSColumn + shift.enumOrdinal;
+  }
+
+  lateDS(shift: Shift): number {
+    return this.firstLateDSColumn + Math.trunc(shift.enumOrdinal / 2);
+  }
+
+  private static findColumn(sheet: Excel.Worksheet, label: string): number {
+    const values = sheet.getRow(1).values as CellValue[];
+    return values.findIndex(value => value === label);
+  }
+}
+
+class Shift extends Enumify {
+  static MONDAY_AM = new Shift();
+  static MONDAY_PM = new Shift();
+  static TUESDAY_AM = new Shift();
+  static TUESDAY_PM = new Shift();
+  static WEDNESDAY_AM = new Shift();
+  static WEDNESDAY_PM = new Shift();
+  static THURSDAY_AM = new Shift();
+  static THURSDAY_PM = new Shift();
+  static FRIDAY_AM = new Shift();
+  static FRIDAY_PM = new Shift();
+  static _ = Shift.closeEnum();
 }
 
 class Team extends Enumify {
