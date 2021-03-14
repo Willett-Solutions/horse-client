cebc21db7460e7b7705572a70620a76839d4437e
david <david.m.willett@gmail.com>
2021-03-14 14:13:53 +0000

    Tidying.

--- src/components/model/rota/field.ts
+++ src/components/model/rota/field.ts
@@ -4,6 +4,84 @@ import Excel from "exceljs";
 import {Enumify} from "enumify";
 import {Duty} from "../domain";
 
+
+abstract class Field {
+  private readonly themeColors: string[]
+  private readonly cell: Excel.Cell;
+
+  constructor(themeColors: string[], cell: Excel.Cell) {
+    this.themeColors = themeColors;
+    this.cell = cell;
+  }
+
+  get content() {
+    return this.cell.text
+  }
+
+  private _color: string | null | undefined;
+
+  protected get color(): string | null {
+    if (this._color === undefined) {
+      this._color = this.getColor();
+    }
+    return this._color;
+  }
+
+  protected set color(value) {
+    assert(value !== null);
+    this.cell.style = {
+      ...this.cell.style,
+      fill: {
+        type: "pattern",
+        pattern: "solid",
+        fgColor: {argb: value},
+      }
+    }
+    this._color = value;
+  }
+
+  private getColor(): string | null {
+    const fill = this.cell.fill;
+    if (fill.type !== "pattern") return null;
+    switch (fill.pattern) {
+      case "solid":
+        return this.getSolidColor(fill.fgColor);
+      case "none":
+        return "FFFFFFFF";
+      default:
+        return null;
+    }
+  }
+
+  private getSolidColor(fgColor: Partial<Excel.Color>) {
+    if (fgColor.hasOwnProperty("argb")) return fgColor.argb ?? null;
+    if (fgColor.hasOwnProperty("theme")) {
+      assert (fgColor.theme === 0 || fgColor.theme === 1);
+      const color = this.themeColors[fgColor.theme];
+      if (fgColor.hasOwnProperty("tint")) {
+        // @ts-ignore
+        const tint = fgColor.tint;
+        const hsl = convert.hex.hsl(color);
+        if (tint < 0) {
+          hsl[2] = hsl[2] * (1 + tint);
+        } else if (tint > 0) {
+          hsl[2] = hsl[2] * (1 - tint) + (255 - 255 * (1 - tint));
+        }
+        return "FF" + convert.hsl.hex(hsl);
+      } else {
+        return "FF" + color;
+      }
+    }
+    return null;
+  }
+}
+
+
+export class TextField extends Field {
+
+}
+
+
 export enum Status {
   AVAILABLE,
   UNAVAILABLE,
@@ -12,84 +90,24 @@ export enum Status {
   DOES_NOT_WORK
 }
 
-abstract class Field {
-  protected themeColors: string[]
-  protected cell: Excel.Cell;
-
-  constructor(themeColors: string[], cell: Excel.Cell) {
-    this.cell = cell;
-    this.themeColors = themeColors;
-  }
-}
-
-export class TextField extends Field {
-  get content(): string {
-    return this.cell.text;
-  }
-}
 
 export class ShiftField extends Field {
-  private getColor(): string | undefined {
-    const fill: Excel.Fill = this.cell.fill;
-    if (fill.type === "pattern") {
-      switch (fill.pattern) {
-        case "none":
-          return "FFFFFFFF";
-        case "solid": {
-          const fgColor = fill.fgColor;
-          if (fgColor.hasOwnProperty("argb")) {
-            return fgColor.argb;
-          } else if (fgColor.hasOwnProperty("theme")) {
-            assert (fgColor.theme === 0 || fgColor.theme === 1);
-            const color = this.themeColors[fgColor.theme];
-            if (fgColor.hasOwnProperty("tint")) {
-              // @ts-ignore
-              const tint = fgColor.tint;
-              const hsl = convert.hex.hsl(color);
-              if (tint < 0) {
-                hsl[2] = hsl[2] * (1 + tint);
-              } else if (tint > 0) {
-                hsl[2] = hsl[2] * (1 - tint) + (255 - 255 * (1 - tint));
-              }
-              return "FF" + convert.hsl.hex(hsl);
-            } else {
-              return "FF" + color;
-            }
-          } else {
-            return undefined;
-          }
-        }
-        default:
-          return undefined;
-      }
-    } else {
-      return undefined;
-    }
-  }
 
   get colorCode(): ColorCode | undefined {
-    const color = this.getColor();
-    if (color === undefined) return undefined;
+    const color = this.color;
+    if (color === null) return undefined;
     let colorCode = ColorCode.fromColor(color);
     if (colorCode !== undefined) return colorCode;
     if (ShiftField.isShadeOfGray(color)) return ColorCode.ANNUAL_LEAVE;
     return undefined;
   }
 
-  private static isShadeOfGray(color: string) {
-    return convert.hex.rgb(color).every((val, i, arr) => val === arr[0]);
+  set colorCode(value) {
+    this.color = value!.color;
   }
 
-  set colorCode(value) {
-    const color = value!.color;
-    this.cell.style = {
-      ...this.cell.style,
-      fill: {
-        type: "pattern",
-        pattern: "solid",
-        fgColor: {argb: color},
-      }
-    }
+  private static isShadeOfGray(color: string) {
+    return convert.hex.rgb(color).every((val, i, arr) => val === arr[0]);
   }
 
   get status(): Status {
@@ -105,7 +123,7 @@ export class ShiftField extends Field {
       case ColorCode.DUTY_LATE_DS:
       case ColorCode.DUTY_SS:
       case ColorCode.UNASSIGNED:
-        if (this.cell.text === "A/L") {
+        if (this.content === "A/L") {
           return Status.ANNUAL_LEAVE;
         } else {
           return Status.AVAILABLE;
@@ -150,11 +168,13 @@ export class ShiftField extends Field {
   }
 }
 
+
 export class AvailabilityField extends Field {
   get available(): boolean {
-    return this.cell.text === "Y";
+    return this.content === "Y";
   }
 }
+
 
 class ColorCode extends Enumify {
   readonly color: string;
